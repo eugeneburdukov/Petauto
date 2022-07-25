18eac5259f83309bf0b6d548a54cc5f2830599f3
Eugene Burdukov <74274237+eugeneburdukov@users.noreply.github.com>
2022-07-25 21:31:32 +0300

    updated

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -7,25 +7,32 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FirstTest {
+    String driverPath = "src/main/resources/chromedriver10305060134";
+    String mainPage = "https://next.privat24.ua/?lang=en";
+    String buttonLoginxPath = "//button[@data-qa-node='login']";
+    String authFramexPath = "//iframe[@src='https://login-widget.privat24.ua/']";
+    String buttonContinuexPath = "//button[@type = 'submit' and contains (text(), 'Continue')]";
+    String errorMessage = "//div[contains(text(), 'Incorrect value was entered')]";
+
     @Test
     public void privatB() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver10305060134");
+        System.setProperty("webdriver.chrome.driver", driverPath);
         WebDriver driver = new ChromeDriver();
 
-        WebDriverWait wait = new WebDriverWait(driver, 1);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
 
-        driver.navigate().to("https://next.privat24.ua/?lang=en");
-        WebElement buttonClick = wait.until(ExpectedConditions.presenceOfElementLocated(
-                By.xpath("//button[@data-qa-node='login']")));
-        buttonClick.click();
+        driver.navigate().to(mainPage);
+        WebElement buttonLogin = wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath(buttonLoginxPath)));
+        buttonLogin.click();
 
-        WebElement authFrame = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//iframe[@src='https://login-widget.privat24.ua/']")));
+        WebElement authFrame = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(authFramexPath)));
         driver.switchTo().frame(authFrame);
 
-        WebElement buttonContinue = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@type = 'submit' and contains (text(), 'Continue')]")));
+        WebElement buttonContinue = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(buttonContinuexPath)));
         buttonContinue.click();
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(text(), 'Incorrect value was entered')]")));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(errorMessage)));
 
     }
 }
